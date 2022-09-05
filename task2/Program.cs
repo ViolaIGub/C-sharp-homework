2c8ba70722f479b455bcff4b5e8753ea4c01f1d4
Viola <viola.gubaidullina@yandex.ru>
2022-09-05 23:54:13 +0500

    попытались создать универсальную программу

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первую переменную");
+string name a = Console.ReadLine();
+Console.WriteLine("Введите вторую переменную");
+string name b = Console.ReadLine();
+
+if (a > b)
+{
+    Console.WriteLine(name a);
+}
+else 
+{
+    Console.WriteLine(name b);
+}
