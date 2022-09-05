62d893cea59ee2d77e7d0971f0f734d126856251
Viola <viola.gubaidullina@yandex.ru>
2022-09-05 23:53:01 +0500

    решили задачу 2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,4 +1,12 @@
-﻿int numberA=3;
-int numberB=5;
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿int a = 5;
+
+int b = 7;
+
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else 
+{
+    Console.WriteLine(b);
+}
