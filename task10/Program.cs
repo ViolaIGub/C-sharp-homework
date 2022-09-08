ddbac130d0b369002d876632841b6ff03c73d86e
Viola <viola.gubaidullina@yandex.ru>
2022-09-08 22:28:58 +0500

    написали код для задачи 10

---
+++ task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число");
+string inputString = Console.ReadLine();
+if (inputString.Length ! = 3)
+{
+    int Number = int.Parse(inputString);
+    Console.WriteLine(("Второе число: ") + (Number/10)%10);
+}
+else 
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
