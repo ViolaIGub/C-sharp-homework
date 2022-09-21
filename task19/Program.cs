e04f2a4b805a48ca85c3eb0531bf30064aaaee77
Viola <viola.gubaidullina@yandex.ru>
2022-09-21 22:10:26 +0500

    решили задачу 19 к Уроку 3

---
+++ task19/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+using System;
+
+int n, reversed = 0, remainder, original;
+
+Console.Write("Введите целое число:");
+n = Convert.ToInt64(Console.ReadLine());
+original = n;
+
+while( n != 0 )
+{
+    remainder = n % 10;
+    reversed = reversed * 10 + remainder;
+    n = 10;
+}
+
+if(original == reversed)
+{
+    Console.WriteLine(original + " палиндром");
+}
+else 
+{
+    Console.WriteLine(original + " не палиндром");
+}
