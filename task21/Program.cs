01d1479b30cc86606b02a4df3a9edf2dd5e64605
Viola <viola.gubaidullina@yandex.ru>
2022-09-24 18:24:20 +0500

    решили задачу 21 к Уроку 3

---
+++ task21/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве.
+
+using System;
+double x1 = 7, x2 = 1, y1 = -5, y2 = -1, z1 = 0, z2 = 9;
+double [] array1 = {x1, y1, z1}, array2 = {x2, y2, z2};
+double destination;
+double square = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1);
+
+destination = Math.Sqrt(square);
+
+Console.WriteLine("Расстояние между точками равно " + destination);
