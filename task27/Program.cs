2eeb3ff637a2180952cb7ab77da5ede69c30dab1
Viola <viola.gubaidullina@yandex.ru>
2022-09-24 23:24:07 +0500

    решили задачу 27 к Уроку 4

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+using System;
+
+int a, b, c;//, d;
+int sum = 0;
+
+Console.Write("Введите любое натуральное число А");
+Console.WriteLine();
+a = Convert.ToInt32(Console.ReadLine());
+
+b = (a % 10);
+c = ((a % 100) - b)/10;
+//d = ((a - (a % 100))/100); 
+sum = b + c;// + d;
+
+Console.WriteLine("Сумма чисел в числе А равна " + sum);
