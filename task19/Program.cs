d895102cb374258f325c2ceb91d4f55a333b1656
Viola <viola.gubaidullina@yandex.ru>
2022-09-24 18:23:45 +0500

    решили задачу 19 к Уроку 3

--- task19/Program.cs
+++ task19/Program.cs
@@ -3,18 +3,24 @@
 
 using System;
 
-int a, b, d;
+int a, b, d, e, f;
 
-Console.Write("Введите первое число из трехзначного числа:");
+Console.Write("Введите первое число из пятизначного числа:");
 a = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число из трехзначного числа:");
+Console.Write("Введите второе число из пятизначного числа:");
 b = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите третье число из трехзначного числа:");
+Console.Write("Введите третье число из пятизначного числа:");
 d = Convert.ToInt32(Console.ReadLine());
 
-if(a == d)
+Console.Write("Введите четвертое число из пятизначного числа:");
+e = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите пятое число из пятизначного числа:");
+f = Convert.ToInt32(Console.ReadLine());
+
+if(a == f || b == e)
 {
     Console.WriteLine("Число - палиндром");
 }
