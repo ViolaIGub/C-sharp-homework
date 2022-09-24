a0e65d8dccce45a0da20b656cc55f5db520df4ec
Viola <viola.gubaidullina@yandex.ru>
2022-09-24 23:23:49 +0500

    решили задачу 29 к Уроку 4

---
+++ task29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+using System;
+
+int [] array =  {1, 2, 5, 7, 19, 6, 1, 33};
+int index; 
+int n = array.Length;
+index = 0;
+n = 8;
+
+while (index < n)
+{
+    Console.WriteLine("Значение " + index + " -го элемента массива: " + array [index]);
+index++;
+}
