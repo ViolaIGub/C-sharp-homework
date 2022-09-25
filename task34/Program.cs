efead77f0761bcb1d62e883cfab4b54acf9f58d5
Viola <viola.gubaidullina@yandex.ru>
2022-09-25 22:24:13 +0500

    решили задачу 34 к УРОКУ 5

---
+++ task34/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+using System;
+
+void FillArray(int [] collection);
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection [index] = new Random().Next(100, 999);
+        index++;
+    }
+}
+
+void PrintArray(int [] col);
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int array = new int[10];
+
+FillArray(array);
+PrintArray(array);
