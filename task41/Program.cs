c49a7215895af154fe9ac1c0d3cdee59acb0eb14
Viola <viola.gubaidullina@yandex.ru>
2022-09-26 23:19:32 +0500

    решили задачу 41 к УРОКУ 6

---
+++ task41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 
+// ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+using System;
+
+int a = 0, b = 7, c = 8, d = -2, e = -2;
+int [] array = {a, b, c, d, e};
+int sum = 0;
+int index;
+
+for(index = 0; index < 5; index++)
+{
+   if(array[] < 0)
+    sum = sum + index;
+}
+Console.WriteLine(sum);
